7201231b90eac180fee8dfe42c456c6a4c805e0c
USER <USER@192.168.0.10>
2020-03-01 22:05:44 +0530

    updated demo class

---
+++ Vinoddemoproject/src/Demoprograms.java
@@ -0,0 +1,11 @@
+
+public class Demoprograms {
+static int i =10;
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+       int k =20;
+       System.out.println(i);
+	}
+
+}
